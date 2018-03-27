20d3f80d64cf1437e90820f5b42dfe5494cc9dbe
Cillian Myles <mylsey4thewin@gmail.com>
2018-03-27 12:09:10 +0100

    Dummy data for testing;

---
+++ app/src/main/java/com/example/contactexporter/DummyData.java
@@ -0,0 +1,61 @@
+package com.example.contactexporter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DummyData {
+
+    public static Contact random() {
+        double lRandom = Math.random();
+        if (lRandom <= 0.33d) {
+            return ELON;
+        } else if (lRandom > 0.66d) {
+            return DONALD;
+        } else {
+            return HILARY;
+        }
+    }
+
+    public static List<Contact> list() {
+        List<Contact> lContacts = new ArrayList<>();
+        lContacts.add(ELON);
+        lContacts.add(HILARY);
+        lContacts.add(DONALD);
+        return lContacts;
+    }
+
+    private static final Contact ELON;
+    private static final int ID_ELON = 1;
+
+    static {
+        Map<String, String> lData = new HashMap<>();
+        lData.put("first_name", "Elon");
+        lData.put("last_name", "Musk");
+        lData.put("company", "SpaceX");
+        ELON = new Contact(ID_ELON, lData);
+    }
+
+    private static final Contact DONALD;
+    private static final int ID_DONALD = 2;
+
+    static {
+        Map<String, String> lData = new HashMap<>();
+        lData.put("first_name", "Donald");
+        lData.put("last_name", "Trump");
+        lData.put("company", "USA");
+        DONALD = new Contact(ID_DONALD, lData);
+    }
+
+    private static final Contact HILARY;
+    private static final int ID_HILARY = 3;
+
+    static {
+        Map<String, String> lData = new HashMap<>();
+        lData.put("first_name", "Hilary");
+        lData.put("last_name", "Clinton");
+        lData.put("company", "Unknown");
+        HILARY = new Contact(ID_HILARY, lData);
+    }
+}
